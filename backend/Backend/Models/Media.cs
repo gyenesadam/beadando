029896d770441350623c230f1ebf1c504434d93a
gyenesadam <wgyenesadam@gmail.com>
2021-06-05 20:54:54 +0200

    Elso

---
+++ backend/Backend/Models/Media.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.Models
+{
+    public class Media
+    {
+        public int MediaID { get; set; }
+        public string Tipus { get; set; }
+        public string Szerzo { get; set; }
+        public string Cim { get; set; }
+        public string Statusz { get; set; }
+
+    }
+}
