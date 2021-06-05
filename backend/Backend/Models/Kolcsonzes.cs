009615a5f984fa8cd0d025eec26cd634f59ff2a4
gyenesadam <wgyenesadam@gmail.com>
2021-06-05 21:14:16 +0200

    Koclsonzes.cs és TagokController.cs

---
+++ backend/Backend/Models/Kolcsonzes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.Models
+{
+    public class Kolcsonzes
+    {
+        public int KolcsonzesID { get; set; }
+        public int TagID { get; set; }
+        public int MediaID { get; set; }
+        public string SZIG { get; set; }
+        public string Kivetel { get; set; }
+        public string Visszavetel { get; set; }
+    }
+}
