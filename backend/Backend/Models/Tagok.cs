2ea741985c3912a84d158c8923440cd59d1802ca
gyenesadam <wgyenesadam@gmail.com>
2021-06-05 21:05:39 +0200

    Tagok.cs

---
+++ backend/Backend/Models/Tagok.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.Models
+{
+    public class Tagok
+    {
+        public int TagID { get; set; }
+        public string Nev { get; set; }
+        public int Telefon { get; set; }
+        public string SZIG { get; set; }
+        public string Lakcim { get; set; }
+        public string Statusz { get; set; }
+
+    }
+}
+}
